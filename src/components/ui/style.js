65e8926a25cf722b05da7139f14f9bb062b93d62
ibrahim <aibrahim3546@gmail.com>
2019-04-08 16:28:08 +0800

    add edit button

--- src/components/ui/style.js
+++ src/components/ui/style.js
@@ -12,7 +12,22 @@ const PageLabel = styled.div`
 	font-weight: bold;
 `;
 
+const Button = styled.span`
+	margin-top: 10px;
+	padding: 10px 15px;
+	color: ${props => props.theme.secondaryColor};
+	text-align: center;
+	font-weight: bold;
+	background-color: ${props => props.bgColor};
+	font-size: 14px;
+	position: relative;
+	top: ${props => props.primary ? '5px' : '0'};
+	cursor: pointer;
+	box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
+`;
+
 export {
 	Wrapper,
 	PageLabel,
+	Button,
 };
